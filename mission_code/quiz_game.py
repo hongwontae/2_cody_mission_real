b0dd0a3e00edac754296ec00106e9f8891f9cb90
dnjsxoghd <feelchok1234@gmail.com>
2026-08-08 16:29:04 +0900

    load_state, save_state

--- mission_code/quiz_game.py
+++ mission_code/quiz_game.py
@@ -1,7 +1,7 @@
 
 
 import json
-
+from quiz import Quiz
 
 class QuizGame :
 
@@ -16,6 +16,15 @@ class QuizGame :
             3 : "test"
         }
         self.load_state()
+
+    def init_quizzes(self) :
+        self.quizzes = [
+            Quiz("대한민국의 수도는?",["서울", "부산", "대구", "인천"],1),
+            Quiz("파이썬의 창시자는?", ["귀도 반 로섬", "제임스 고슬링", "비야네", "데니스 리치"],1),
+            Quiz("축구 감독인 사람은?", ["아카자", "홍명보", "펩시콜라", "펩"], 4),
+            Quiz("코디세이의 위치는?", ["평양", "쓰촨성", "약간포동", "개포동"], 4),
+            Quiz("학습 네이토의 정체는?", ["깡통", "할루시네이션", "AI Chat Bot", "네이트"],3),
+        ]
 
     def show_menu(self):
             print("""
@@ -73,8 +82,43 @@ Quiz Game
         try :
             with open("state.json", "r", encoding="utf-8") as f:
                 data = json.load(f)
-        except :
-            pass
 
+            self.best_score = data["best_score"]
+            self.history = data.get("history", [])
+            self.quizzes = []
+
+            for quiz_data in data["quizzes"]:
+                quiz = Quiz(
+                    quiz_data["question"],
+                    quiz_data["choices"],
+                    quiz_data["answer"],
+                    )
+
+                self.quizzes.append(quiz)
+
+        except FileNotFoundError:
+            print("저장 파일이 없습니다. 기본 퀴즈를 생성합니다.")
+            self.init_quizzes()
+            self.save_state()
+
+        except json.JSONDecodeError:
+            print("저장 파일이 손상되었습니다. 기본 퀴즈로 복구합니다.")
+            self.init_quizzes()
+            self.save_state()
+        except KeyError :
+            print("저장할 파일 형식이 올바르지 않습니다.")
+            self.init_quizzes()
+            self.save_state()
+
+    def save_state(self):
+        data = {
+                "best_score": self.best_score,
+                "quizzes": [quiz.to_dict() for quiz in self.quizzes],
+                "history": self.history}
+        try :
+            with open("state.json", "w", encoding="utf-8") as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
+        except OSError :
+            print("파일 저장에 실패했습니다.")
 
 
